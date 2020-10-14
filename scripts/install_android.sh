420a9a442842227a975ba443b50e4302901c2951
MarcosLopez99 <marcosleonor99@gmail.com>
2020-10-14 17:14:44 -0600

    Add documentation to scripts

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -4,11 +4,19 @@ set -x
 
 # Dependencies
 yarn add react-native-camera@git+https://git@github.com/react-native-community/react-native-camera.git
+react-native link react-native-camera
 yarn add react-native-qrcode-scanner
 yarn add react-native-permissions
+react-native link react-native-permissions
+
+#For the rn-camera and rn-permission libraries the react-native link is used, although the new versions of RN come with Auto-linking these libraries do not do the automatic linking
+
+#build.gradle
 
 sed -i.bak '/defaultConfig {/a\
-\     missingDimensionStrategy 'react-native-camera', 'general'
+\     missingDimensionStrategy "react-native-camera", "general"
 ' android/app/build.gradle
+
+#In case there is an error when installing the script, change the "" to ''
 
 echo "configured Android settings"
--- scripts/install_ios.sh
+++ scripts/install_ios.sh
@@ -9,16 +9,20 @@ yarn add react-native-qrcode-scanner
 yarn add react-native-permissions
 react-native link react-native-permissions
 
+#For the rn-camera and rn-permission libraries the react-native link is used, although the new versions of RN come with Auto-linking these libraries do not do the automatic linking
+
 # Podfile
 cd ios
 
 sed -i.bak '/RNCPushNotificationIOS/a\
-  \  pod 'Permission-Camera', :path => "#{permissions_path}/Camera.podspec"
+  \  pod "Permission-Camera", :path => "#{permissions_path}/Camera.podspec"
   ' Podfile\
 
 sed -i.bak '/RNCPushNotificationIOS/a\
-  \  permissions_path = '../node_modules/react-native-permissions/ios'
+  \  permissions_path = "../node_modules/react-native-permissions/ios"
   ' Podfile
+
+#In case there is an error when installing the script, change the "" to ''
 
 pod install && cd ..
 
